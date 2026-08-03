819bb6dae38b64fb4980fc4f61906ef70715b2e4
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-03 20:00:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import requests
 
-print("Forex Intelligence Bot Started")
+TOPIC = "forex_pai_2026"
+
+message = """
+🤖 Forex Intelligence Bot
+
+✅ Python berhasil dijalankan
+✅ GitHub Actions berhasil
+✅ Siap untuk mengambil data ekonomi
+"""
+
+requests.post(
+    f"https://ntfy.sh/{TOPIC}",
+    data=message.encode("utf-8"),
+    headers={
+        "Title": "Forex Intelligence",
+        "Priority": "default"
+    }
+)
+
+print("Notification sent!")
