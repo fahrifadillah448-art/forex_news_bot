a62dec51d991c131c0b2393ae63cccd84c7a1850
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-03 20:29:21 +0700

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,19 @@
+def get_events():
+    return [
+        {
+            "country": "🇺🇸",
+            "title": "Initial Jobless Claims",
+            "time": "19:30 WIB",
+            "forecast": "236K",
+            "previous": "233K",
+            "impact": "🔥🔥🔥"
+        },
+        {
+            "country": "🇺🇸",
+            "title": "Core PCE",
+            "time": "20:30 WIB",
+            "forecast": "2.8%",
+            "previous": "2.7%",
+            "impact": "🔥🔥🔥"
+        }
+    ]
