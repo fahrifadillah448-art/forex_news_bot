0a3655b0504711f423c456d614a1e074057d6f28
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-04 07:37:32 +0700

    Update economic_calendar.py

--- economic_calendar.py
+++ economic_calendar.py
@@ -1,15 +1,48 @@
+import requests
+from config import TICKATLAS_API_KEY
 from provider import EconomicProvider
 
 class EconomicCalendar(EconomicProvider):
 
     def get_events(self):
-        return [
-            {
+
+        url = "https://tickatlas.com/v1/calendar"
+
+        headers = {
+            "X-API-Key": TICKATLAS_API_KEY
+        }
+
+        params = {
+            "currencies": "USD",
+            "impact": "high",
+            "next_hours": 24
+        }
+
+        response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            timeout=20
+        )
+
+        if response.status_code != 200:
+            print(response.status_code)
+            print(response.text)
+            return []
+
+        data = response.json()
+
+        events = []
+
+        for item in data["data"]["events"]:
+
+            events.append({
                 "country": "🇺🇸",
-                "title": "Provider Ready",
-                "time": "--",
-                "forecast": "--",
-                "previous": "--",
+                "title": item["event"],
+                "time": item["datetime"],
+                "forecast": item.get("forecast", "-"),
+                "previous": item.get("previous", "-"),
                 "impact": "🔥"
-            }
-        ]
+            })
+
+        return events
