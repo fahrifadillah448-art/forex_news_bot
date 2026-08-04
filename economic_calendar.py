d9740efc18950fb4c09f4a0fda24a96ceb0d7d0b
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-04 17:09:52 +0700

    Update economic_calendar.py

--- economic_calendar.py
+++ economic_calendar.py
@@ -35,39 +35,51 @@ class EconomicCalendar(EconomicProvider):
 
         events = []
 
+        now = datetime.now(ZoneInfo("UTC"))
+
         for item in data["data"]["events"]:
 
-            # UTC -> WIB
             utc_time = datetime.fromisoformat(
                 item["datetime"].replace("Z", "+00:00")
             )
+
+            minutes_left = (
+                utc_time - now
+            ).total_seconds() / 60
+
+            # hanya kirim jika 20-30 menit sebelum berita
+            if minutes_left > 30:
+                continue
+
+            if minutes_left < 20:
+                continue
 
             wib_time = utc_time.astimezone(
                 ZoneInfo("Asia/Jakarta")
             )
 
-            formatted_time = wib_time.strftime("%d %B %Y | %H:%M WIB")
+            formatted_time = wib_time.strftime(
+                "%d %B %Y | %H:%M WIB"
+            )
 
-            # Impact
             impact = item.get("impact", "").lower()
 
             if impact == "high":
-                impact = "🔥🔥🔥 High"
+                impact = "🔥🔥🔥 High Impact"
             elif impact == "medium":
-                impact = "🔥🔥 Medium"
-            elif impact == "low":
-                impact = "🔥 Low"
+                impact = "🔥🔥 Medium Impact"
             else:
-                impact = "-"
+                impact = "🔥 Low Impact"
 
             events.append({
-    "id": f"{item.get('currency')}_{item.get('event')}_{item.get('datetime')}",
-    "country": f"🇺🇸 {item.get('currency', 'USD')}",
-    "title": item.get("event", "Unknown Event"),
-    "time": formatted_time,
-    "forecast": item.get("forecast") or "-",
-    "previous": item.get("previous") or "-",
-    "actual": item.get("actual") or "-",
-    "impact": impact
-})
+                "country": f"🇺🇸 {item.get('currency','USD')}",
+                "title": item.get("event","Unknown Event"),
+                "time": formatted_time,
+                "forecast": item.get("forecast") or "-",
+                "previous": item.get("previous") or "-",
+                "actual": item.get("actual") or "-",
+                "impact": impact,
+                "minutes_left": int(minutes_left)
+            })
+
         return events
