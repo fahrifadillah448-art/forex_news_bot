0dabe44b0324071022d22d46c3e9d577b22330cb
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-06 10:29:55 +0700

    Update supabase_client.py

--- database/supabase_client.py
+++ database/supabase_client.py
@@ -1,7 +1,23 @@
 from supabase import create_client
 from config import SUPABASE_URL, SUPABASE_KEY
 
-supabase = create_client(
-    SUPABASE_URL,
-    SUPABASE_KEY
-)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+def event_exists(event_id):
+    response = (
+        supabase.table("sent_events")
+        .select("id")
+        .eq("event_id", event_id)
+        .execute()
+    )
+
+    return len(response.data) > 0
+
+
+def save_event(event):
+    supabase.table("sent_events").insert({
+        "event_id": event["event_id"],
+        "title": event["title"],
+        "event_time": event["time"]
+    }).execute()
