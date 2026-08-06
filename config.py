678695c1e9da1c970aa4fddb1080b7271739b81b
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-06 09:47:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
-TOPIC = "forex_pai_2026"
-
 TICKATLAS_API_KEY = os.getenv("TICKATLAS_API_KEY")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+TOPIC = "forex_pai_2026"
