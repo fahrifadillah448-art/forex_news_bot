51a95826c64ea2667bee33274b2fce7241ff18ad
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-07 14:09:48 +0700

    Update logger.py

--- database/logger.py
+++ database/logger.py
@@ -1,6 +1,19 @@
 from database.supabase_client import supabase
 
-response = supabase.table("bot_logs").select("*").limit(1).execute()
 
-print("SUPABASE TEST")
-print(response.data)
+def log_info(message):
+    response = supabase.table("bot_logs").insert({
+        "level": "INFO",
+        "message": message
+    }).execute()
+
+    return response.data
+
+
+def log_error(message):
+    response = supabase.table("bot_logs").insert({
+        "level": "ERROR",
+        "message": message
+    }).execute()
+
+    return response.data
