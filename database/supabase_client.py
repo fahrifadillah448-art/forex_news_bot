f9cbbf3138341f0143c6bb698ba288edc1adda1d
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-07 16:55:10 +0700

    Update supabase_client.py

--- database/supabase_client.py
+++ database/supabase_client.py
@@ -1,14 +1,21 @@
 from supabase import create_client
 from config import SUPABASE_URL, SUPABASE_KEY
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
 
 
 def event_exists(event_id):
+
     response = (
-        supabase.table("sent_events")
-        .select("id")
+        supabase
+        .table("sent_events")
+        .select("event_id")
         .eq("event_id", event_id)
+        .limit(1)
         .execute()
     )
 
@@ -16,8 +23,37 @@ def event_exists(event_id):
 
 
 def save_event(event):
-    supabase.table("sent_events").insert({
-        "event_id": event["event_id"],
-        "title": event["title"],
-        "event_time": event["time"]
-    }).execute()
+
+    result = (
+        supabase
+        .table("sent_events")
+        .insert({
+            "event_id": event["event_id"],
+            "title": event["title"],
+            "event_time": convert_to_timestamp(
+                event["time"]
+            )
+        })
+        .execute()
+    )
+
+    return result
+
+
+def convert_to_timestamp(time_string):
+
+    # Input:
+    # 2026-08-07 19:30 WIB
+    #
+    # Output:
+    # 2026-08-07T19:30:00+07:00
+
+    clean_time = time_string.replace(
+        " WIB",
+        ""
+    )
+
+    return clean_time.replace(
+        " ",
+        "T"
+    ) + "+07:00"
