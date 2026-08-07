372afd66bf4d9b59315cada18bf8392aeb37201e
fahrifadillah448-art <fahrifadillah448@gmail.com>
2026-08-07 14:33:13 +0700

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,15 +1,36 @@
 import requests
+
 from config import TOPIC
 
-def send_notification(title, message):
-    response = requests.post(
-        f"https://ntfy.sh/{TOPIC}",
-        data=message.encode("utf-8"),
-        headers={
-            "Title": title,
-            "Priority": "default"
-        }
-    )
 
-    print("Notify Status:", response.status_code)
-    print("Notify Response:", response.text)
+def send_notification(title, message):
+
+    url = f"https://ntfy.sh/{TOPIC}"
+
+    try:
+
+        response = requests.post(
+            url,
+            data=message.encode("utf-8"),
+            headers={
+                "Title": title,
+                "Priority": "default"
+            },
+            timeout=20
+        )
+
+        print("Notify Status:", response.status_code)
+        print("Notify Response:", response.text)
+
+        # HTTP selain 2xx dianggap gagal
+        response.raise_for_status()
+
+        print("Notification sent successfully")
+
+        return True
+
+    except requests.exceptions.RequestException as e:
+
+        print("Notification failed:", str(e))
+
+        raise
